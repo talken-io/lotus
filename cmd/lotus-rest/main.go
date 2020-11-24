3ab68446430cc39993309ff176fbd265608b131e
Peter H. Nahm <hwnahm@gmail.com>
2020-11-24 13:13:53 +0900

    lotus-rest Multi-Thread 적용

--- cmd/lotus-rest/main.go
+++ cmd/lotus-rest/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
+	"net"
 	"net/http"
+	"strconv"
+	"sync"
+	"time"
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/gorilla/mux"
@@ -33,6 +36,42 @@ func convertMessage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type tcpKeepAliveListener struct {
+	*net.TCPListener
+}
+
+func (ln tcpKeepAliveListener) Accept() (c net.Conn, err error) {
+	tc, err := ln.AcceptTCP()
+	if err != nil {
+		return
+	}
+	tc.SetKeepAlive(true)
+	tc.SetKeepAlivePeriod(3 * time.Minute)
+	return tc, nil
+}
+
+func ListenAndServe(addr string, num int, r http.Handler) error {
+	if addr == "" {
+		addr = ":http"
+	}
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+	var wg sync.WaitGroup
+	for i := 0; i < num; i++ {
+		wg.Add(1)
+		go func(i int) {
+			log.Warnf("listener number", i)
+			log.Fatal(http.Serve(tcpKeepAliveListener{ln.(*net.TCPListener)}, r))
+
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	return nil
+}
+
 func main() {
 	ip := flag.String("ip", "127.0.0.1", "Listening IP")
 	port := flag.Int("port", 3030, "Listening Port")
@@ -41,6 +80,12 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/convertMessage", convertMessage).Methods("POST")
 
-	server := fmt.Sprintf("%s:%d", *ip, *port)
-	log.Fatal(http.ListenAndServe(server, r))
+	//server := fmt.Sprintf("%s:%d", *ip, *port)
+	//log.Fatal(http.ListenAndServe(server, r))
+	log.Fatal(http.ListenAndServe(*ip+":"+strconv.Itoa(*port), r))
+
+	//// Multi-Thread
+	//num := runtime.NumCPU()
+	//runtime.GOMAXPROCS(num)
+	//log.Fatal(ListenAndServe(*ip + ":" + strconv.Itoa(*port), num, r))
 }
